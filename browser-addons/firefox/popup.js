c88ffac5a2ce57de834b80828da6978ea303b745
Phil Gersekowski <philg@aspedia.net>
2026-04-25 23:31:44 +1000

    chore: bump version to 1.7.3-rc.10

--- browser-addons/firefox/popup.js
+++ browser-addons/firefox/popup.js
@@ -1,7 +1,7 @@
 // ImageTools Popup Script
 
 // Get build date from manifest (injected at build time)
-const BUILD_DATE = '2026-04-25T13:28:18.180Z';  // Will be updated by build script
+const BUILD_DATE = '2026-04-25T13:31:44.351Z';  // Will be updated by build script
 
 const TAG_KEY = 'imagetools_current_tag';
 
